8da721cba9f4af1160e9c3aee53eedf630f0c55b
Anthony <aschulist@student.neumont.edu>
2016-07-07 15:58:19 -0600

    finish methods crawl, visitURL, and visitAction within the class Webcrawler. visitAction might have not changed but I'm not sure if it worked in it last state since I could test till now.

--- webclawer/src/main/Webcrawler.java
+++ webclawer/src/main/Webcrawler.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -10,30 +11,55 @@ import interfaces.VisitAction;
 
 public class Webcrawler implements VisitAction{
 
-	static String STARTURL = "http://shalladay-iis1.student.neumont.edu/";
+	static String STARTURL = "http://shalladay-iis1.student.neumont.edu";
 	ArrayList<String> beenVisited;
 	ArrayList<String> toVisit;
 	String startingURL;
 	
 	public static void main(String[] args) {
 		Webcrawler crawler = new Webcrawler(STARTURL);
+		crawler.crawl();
+		for(String url: crawler.beenVisited){
+			System.out.println(url);
+		}
 	}
 	
 	public Webcrawler(String startingURL) {
 		// TODO Auto-generated constructor stub
 		this.startingURL = startingURL;
-		toVisit.add(startingURL);
+		toVisit = new ArrayList<String>();
+		beenVisited = new ArrayList<String>();
+		toVisit.add(this.startingURL);
 	}
 	
 	public void crawl(){
 		while(!toVisit.isEmpty()){
 			visitURL();
-			
 		}
 	}
 	
 	public void visitURL(){
-		Iterator<String> childrenUrl = visitAction(null);
+		Iterator<String> childrenUrl = null;
+		try {
+			childrenUrl = visitAction(new URL(toVisit.get(0)));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
+		beenVisited.add(toVisit.get(0));
+		toVisit.remove(0);
+		while(childrenUrl.hasNext()){
+			String childLink = startingURL + childrenUrl.next();
+			if(!beenVisited.contains(childLink)){
+				if(!toVisit.isEmpty()){
+					if(!toVisit.contains(childLink)){
+						toVisit.add(childLink);
+					}
+				} else {
+					toVisit.add(childLink);
+				}
+			}
+		}
 	}
 
 	@Override
@@ -44,7 +70,7 @@ public class Webcrawler implements VisitAction{
 			lf.processPage(url.openStream());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(e);
 		}
 		Iterator<String> links = lf.getLinks();
 		return links;
